68a3a08ce0ac68beed98f129a21239d78ee79fff
kevin Martínez <kevinccbsg@gmail.com>
2020-03-21 20:19:08 +0100

    feat: add descriptiion and social links

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ module.exports = {
   pathPrefix: '/',
   title: 'Blog by John Doe',
   subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
-  copyright: '© All rights reserved.',
+  copyright: '',
   disqusShortname: '',
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: 'UA-161551264-1',
   useKatex: false,
   menu: [
     {
@@ -18,26 +18,20 @@ module.exports = {
     {
       label: 'About me',
       path: '/pages/about'
-    },
-    {
-      label: 'Contact me',
-      path: '/pages/contacts'
     }
   ],
   author: {
-    name: 'John Doe',
-    photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    name: 'Kevin Martínez',
+    photo: '/kevin_martinez.jpg',
+    bio: 'Javascript developer at GuideSmiths. I have the goal to share with you knowledge about testing, cypress, Javascript and everything relate to web technologies.',
     contacts: {
-      email: '',
-      facebook: '#',
-      telegram: '#',
-      twitter: '#',
-      github: '#',
+      email: 'kevinccbsg@gmail.com',
+      twitter: 'https://twitter.com/kjmesc',
+      github: 'https://github.com/kevinccbsg',
       rss: '',
       vkontakte: '',
-      linkedin: '#',
-      instagram: '#',
+      linkedin: 'https://www.linkedin.com/in/kevinjmartinez/',
+      instagram: 'https://www.instagram.com/kevin_jme',
       line: '',
       gitlab: '',
       weibo: '',
